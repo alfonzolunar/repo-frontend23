83ac675c9d3e02ed6e83dfe50393149a61edc5e8
Alfonzo Lunar <102769417+alfonzolunar@users.noreply.github.com>
2024-10-15 19:55:10 -0500

    proyect1.1

--- compartido/static/js/appRegister.js
+++ compartido/static/js/appRegister.js
@@ -7,8 +7,55 @@ function register_user(){
     let fecha = document.getElementById("date").value
     let email = document.getElementById("email").value
     let contrasena = document.getElementById("passw").value
+    let check_accept = document.getElementById("check-accept").checked
 
-    document.write(nombre + " " + apellido + " " + cedula + " " + telefono + " " + fecha
-        + " " + email + " " + contrasena
-    )
+    let confirm_vacio = validar_vacio(nombre, apellido, cedula, telefono, fecha, email, contrasena, check_accept)
+    if (confirm_vacio == true) {
+        alert("Usuario creado")
+    }
+}
+
+function validar_vacio(nombre, apellido, cedula, telefono, fecha, email, contrasena, check_accept) {
+    let confirm_vacio = true
+    if (nombre == "") {
+        alert("El campo nombre no puede estar vacío")
+        confirm_vacio = false
+    }
+
+    if (apellido == "") {
+        alert("El campo apellido no puede estar vacío")
+        confirm_vacio = false
+    }
+
+    if (cedula == "") {
+        alert("El campo cedula no puede estar vacío")
+        confirm_vacio = false
+    }
+
+    if (telefono == "") {
+        alert("El campo telefono no puede estar vacío")
+        confirm_vacio = false
+    }
+    
+    if (fecha == "") {
+        alert("El campo fecha no puede estar vacío")
+        confirm_vacio = false
+    }
+    
+    if (email == "") {
+        alert("El campo email no puede estar vacío")
+        confirm_vacio = false
+    }
+    
+    if (contrasena == "") {
+        alert("El campo contraseña no puede estar vacío")
+        confirm_vacio = false
+    }
+
+    if (check_accept == "") {
+        alert("Debes aceptar los terminos")
+        confirm_vacio = false
+    }
+
+    return confirm_vacio
 }
